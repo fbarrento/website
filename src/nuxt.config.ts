6e6ba8906c1f62f6168957ebb4ab6e1b085a22df
Francisco Barrento <francisco.barrento@gmail.com>
2022-11-05 12:16:57 +0000

    Fixing HTTP2 TLS error

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    experimental: {
+        writeEarlyHints: false,
+    },
 
     modules: [
         "@nuxt/content"
