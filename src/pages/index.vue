78478257c9218d760fe00756143160e8835d8f6b
Francisco Barrento <francisco.barrento@gmail.com>
2022-11-07 22:02:16 +0000

    Deploying new version

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,23 @@
 <script setup>
+
+
+const authors = await queryContent('authors').find()
+
+function getAuthor(path) {
+         return authors.find(
+            author => `/authors/${path}` === author._path
+        )
+    
+    }
+
 const episodes = await queryContent('series')
-  .where({ type: "episode" })
-  .sort({ title: 1 })
-  .find()
+    .where({ type: "episode" })
+    .sort({ title: 1 })
+    .find()
+
+
+
+
 
 </script>
 <template>
@@ -11,20 +26,33 @@ const episodes = await queryContent('series')
             <Hero />
         </template>
     <div>
+        
         <h3 class="text-gray-500 font-medium pb-3">
             recent episodes
         </h3>
         <article
             v-for="episode in episodes"
             :key="episode._id"
-            class="mb-6"
+            class="mb-8"
         >
+            <div class="flex items-center pb-2" >
+                <img :src="getAuthor(episode.author).avatar" class="rounded-full w-7 bg-yellow-500" />
+                <div class="pl-2 text-sm">
+                    <span class="font-semibold">
+                        {{ getAuthor(episode.author).title }}
+                    </span> in 
+                    <span class="font-semibold">
+                        {{ episode.serie }}
+                    </span>
+                </div>
+            </div>
+            
             <h2 class="text-xl font-bold mb-1">
                 <NuxtLink :to="episode._path">
                     {{ episode.title }}
                 </NuxtLink>
             </h2>
-            <p>{{ episode.description }}</p>
+            <p class="text-gray-500">{{ episode.description }}</p>
         </article>
     </div>
 </NuxtLayout>
