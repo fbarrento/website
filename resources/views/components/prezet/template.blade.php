3128f3fe43314b08dcb5195c4b7a437e54934e06
Francisco Barrento <francisco.barrento@gmail.com>
2025-11-02 15:31:32 +0000

    feat: rename content files and add new data factory documentation

--- resources/views/components/prezet/template.blade.php
+++ resources/views/components/prezet/template.blade.php
@@ -24,6 +24,17 @@
     <link rel="alternate" type="application/atom+xml" title="News" href="/feed">
 
     <!-- Scripts -->
+    @env('production')
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l]
+            .push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})
+        (window,document,'script','dataLayer','GTM-TQGPB9FN');
+    </script>
+    <!-- End Google Tag Manager -->
+    @endenv
+
+
     <!-- Zoomable Plugin (example version) -->
     <script defer src="https://unpkg.com/@benbjurstrom/alpinejs-zoomable@0.4.0/dist/cdn.min.js"></script>
     <script
@@ -62,6 +73,12 @@
     <title></title>
 </head>
 <body class="dark:bg-background-dark font-sans antialiased overscroll-none">
+@env('production')
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQGPB9FN"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+@endenv
 <div class="min-h-screen">
     <x-prezet.alpine>
         <div
